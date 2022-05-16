75ad0caac8013944ef5b5386afef1ce697b14206
tsuccar <tsuccar@users.noreply.github.com>
2022-05-16 09:01:06 -0400

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+  //   Perhaps you can finish the alternative way of combinging the two togetther ????
+
+  //  const [state, setState] = useState({
+  //     headingText: "Hello",
+  //     bgColor: "white"
+  //   });
+
+  // function handleClick ()
+  // {
+  //   setState({headingText: "Submitted"})
+  // }
+
+  // function changeColorBlack()
+  // {
+  //   setState({bgColor: "black"})
+  // }
+  // function changeColorWhite()
+  // {
+  //   setState({bgColor: "white"})
+  //  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
